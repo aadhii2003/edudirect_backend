6428a46e9954cd5ee4f9d832f4df44667fb9f9ae
aadhii2414 <adithyanair2003324@gmail.com>
2026-07-31 15:46:38 +0530

    Fix blog image URLs

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
 from .models import BlogPost
 
+
 class BlogPostSerializer(serializers.ModelSerializer):
+    featuredImage = serializers.SerializerMethodField()
+    authorAvatar = serializers.SerializerMethodField()
+
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = "__all__"
+
+    def _build_url(self, file):
+        if not file:
+            return None
+
+        request = self.context.get("request")
+
+        if request:
+            return request.build_absolute_uri(file.url)
+
+        return file.url
+
+    def get_featuredImage(self, obj):
+        return self._build_url(obj.featuredImage)
+
+    def get_authorAvatar(self, obj):
+        return self._build_url(obj.authorAvatar)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
-from rest_framework import permissions
 
 class BlogPostViewSet(viewsets.ModelViewSet):
-    queryset = BlogPost.objects.all().order_by('-date')
+    queryset = BlogPost.objects.all().order_by("-date")
     serializer_class = BlogPostSerializer
-    lookup_field = 'slug'
+    lookup_field = "slug"
 
     def get_permissions(self):
-        if self.request.method == 'GET':
+        if self.request.method == "GET":
             return [permissions.AllowAny()]
         return [permissions.IsAdminUser()]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
